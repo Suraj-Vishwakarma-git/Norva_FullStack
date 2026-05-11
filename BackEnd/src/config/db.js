46420f00295d7a0af6cbe560226d22d48c26f42a
Suraj <ssvsurajvishwakarma@gmail.com>
2026-05-11 22:53:41 +0530

    Removed

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB=async ()=>{
     try{
-       await mongoose.connect("mongodb+srv://ssvsurajvishwakarma_db_user:7058@cluster0.kn1gsi8.mongodb.net/Norva");
         console.log("MongoDB connected");
     }
     catch(e){
