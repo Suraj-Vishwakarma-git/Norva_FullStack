0c5e2d9604bcc6f56e6e9c56b5187193ce28dd60
Suraj <ssvsurajvishwakarma@gmail.com>
2026-05-13 23:03:18 +0530

    Login page

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB=async ()=>{
     try{
+       await mongoose.connect("mongodb+srv://ssvsurajvishwakarma_db_user:7058@cluster0.kn1gsi8.mongodb.net/Norva");
         console.log("MongoDB connected");
     }
     catch(e){
---
+++ FrontEnd/src/components/auth/LoginPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const LoginPage = () => {
+  async function Login() {
+     try{
+        const API=await fetch("",{
+          
+        })
+     }catch(e){
+
+     }
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginPage
