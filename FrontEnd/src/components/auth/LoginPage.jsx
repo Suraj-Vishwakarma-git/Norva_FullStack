a622bd1e3e9efffb3b9b1ff00f1713587df0ed86
Suraj <ssvsurajvishwakarma@gmail.com>
2026-05-16 18:58:54 +0530

    Working on FrontEnd

--- FrontEnd/src/components/auth/LoginPage.jsx
+++ FrontEnd/src/components/auth/LoginPage.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const LoginPage = () => {
+
+ const [email,setEmail]=useState("");
+ const [password,setPassword]=useState("");
+
   async function Login() {
      try{
-        const API=await fetch("",{
-          
+        const API=await fetch("http://localhost:3000/api/auth/login",{
+          method:"POST",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify(email,password)
         })
      }catch(e){
-
+       console.log("Server Error ",e);
      }
   }
   return (
